03387fccd9ae618dab6372ffee4d70c2f10ca44a
Dani-vn18 <juanda.vn18@gmail.com>
2024-03-14 11:01:19 -0500

    CSRF

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = ['localhost','django-railway-production-4b4f.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://django-railway-production-4b4f.up.railway.app']
